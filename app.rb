06fded9abe4c9527b748cbee28fbd1e70703742d
George McRedmond <georgem.int101@gmail.com>
2016-10-18 11:54:12 -0400

    front end upload

--- app.rb
+++ app.rb
@@ -9,3 +9,17 @@ configure :development do
   set :database, 'sqlite3:db/database.db'
 end 
 require './models'
+
+
+$menu = [
+	    {page: 'Home', href: '/'},
+	    {page: 'Profile', href: '/profile'},
+	    {page: 'Account', href: '/account'},
+	    {page: 'Feed', href: '/feed'},
+	    {page: 'Teammates', href: '/teammates'},
+	    {page: 'Sign Out', href: '/sign_out'}
+  	]
+
+  get '/' do 
+ 	erb :home
+ end
